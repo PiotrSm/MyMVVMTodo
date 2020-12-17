e765dd8b78f685cc64a146107ba92e1702d5d9f7
PiotrSm <franko102@interia.pl>
2020-12-17 11:19:29 +0100

    navigation graph and TasksFragments.kt

--- app/src/main/java/com/homeworkshop/mymvvmtodo/ui/MainActivity.kt
+++ app/src/main/java/com/homeworkshop/mymvvmtodo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.homeworkshop.mymvvmtodo
+package com.homeworkshop.mymvvmtodo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.homeworkshop.mymvvmtodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/homeworkshop/mymvvmtodo/ui/tasks/TasksFragments.kt
@@ -0,0 +1,7 @@
+package com.homeworkshop.mymvvmtodo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.homeworkshop.mymvvmtodo.R
+
+class TasksFragments:Fragment(R.layout.fragment_tasks) {
+}
